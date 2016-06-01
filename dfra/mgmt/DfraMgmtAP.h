cc5a6955edecae0450d5f8453fb81b0c4fbbb00f
Daniel Tweed <m.danieltweed@gmail.com>
2016-06-01 10:03:17 -0400

    removed several metered channels

--- dfra/mgmt/DfraMgmtAP.h
+++ dfra/mgmt/DfraMgmtAP.h
@@ -63,6 +63,9 @@ class INET_API DfraMgmtAP : public Ieee80211MgmtAPBase, protected cListener
         int numStations = 0;
         BYTE frameTypes = 0;
         BYTE *staSchedules = nullptr;
+        int size = sizeof(numStations)+sizeof(BYTE)+numStations*sizeof(BYTE);;
+        Sched(){}
+        ~Sched(){delete staSchedules;}
     };
 
 
@@ -152,7 +155,7 @@ class INET_API DfraMgmtAP : public Ieee80211MgmtAPBase, protected cListener
     void sendAssocNotification(const MACAddress& addr);
 
     void sendDisAssocNotification(const MACAddress& addr);
-
+    void finish();
     /** lifecycle support */
     //@{
 
